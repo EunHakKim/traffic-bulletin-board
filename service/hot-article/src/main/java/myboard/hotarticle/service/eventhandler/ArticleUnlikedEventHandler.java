debb41b87fdc5479858faca693d784165f12dce9
EunHakKim <acky529@gmail.com>
2025-02-15 23:07:47 +0900

    인기글 관련 Consumer 개발

---
+++ service/hot-article/src/main/java/myboard/hotarticle/service/eventhandler/ArticleUnlikedEventHandler.java
@@ -0,0 +1,35 @@
+package myboard.hotarticle.service.eventhandler;
+
+import kuke.board.common.event.Event;
+import kuke.board.common.event.EventType;
+import kuke.board.common.event.payload.ArticleUnlikedEventPayload;
+import lombok.RequiredArgsConstructor;
+import myboard.hotarticle.repository.ArticleLikeCountRepository;
+import myboard.hotarticle.utils.TimeCalculatorUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ArticleUnlikedEventHandler implements EventHandler<ArticleUnlikedEventPayload> {
+    private final ArticleLikeCountRepository articleLikeCountRepository;
+
+    @Override
+    public void handle(Event<ArticleUnlikedEventPayload> event) {
+        ArticleUnlikedEventPayload payload = event.getPayload();
+        articleLikeCountRepository.createOrUpdate(
+                payload.getArticleId(),
+                payload.getArticleLikeCount(),
+                TimeCalculatorUtils.calculateDurationToMidnight()
+        );
+    }
+
+    @Override
+    public boolean supports(Event<ArticleUnlikedEventPayload> event) {
+        return EventType.ARTICLE_UNLIKED == event.getType();
+    }
+
+    @Override
+    public Long findArticleId(Event<ArticleUnlikedEventPayload> event) {
+        return event.getPayload().getArticleId();
+    }
+}
---
+++ service/hot-article/src/test/java/myboard/hotarticle/service/HotArticleScoreCalculatorTest.java
@@ -0,0 +1,45 @@
+package myboard.hotarticle.service;
+
+import myboard.hotarticle.repository.ArticleCommentCountRepository;
+import myboard.hotarticle.repository.ArticleLikeCountRepository;
+import myboard.hotarticle.repository.ArticleViewCountRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.random.RandomGenerator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class HotArticleScoreCalculatorTest {
+    @InjectMocks
+    HotArticleScoreCalculator hotArticleScoreCalculator;
+    @Mock
+    ArticleLikeCountRepository articleLikeCountRepository;
+    @Mock
+    ArticleViewCountRepository articleViewCountRepository;
+    @Mock
+    ArticleCommentCountRepository articleCommentCountRepository;
+
+    @Test
+    void calculateTest() {
+        //given
+        Long articleId = 1L;
+        long likeCount = RandomGenerator.getDefault().nextLong(100);
+        long commentCount = RandomGenerator.getDefault().nextLong(100);
+        long viewCount = RandomGenerator.getDefault().nextLong(100);
+        given(articleLikeCountRepository.read(articleId)).willReturn(likeCount);
+        given(articleCommentCountRepository.read(articleId)).willReturn(commentCount);
+        given(articleViewCountRepository.read(articleId)).willReturn(viewCount);
+
+        //when
+        long score = hotArticleScoreCalculator.calculate(articleId);
+
+        //then
+        assertEquals(score, 3 * likeCount + 2 * commentCount + viewCount);
+    }
+}
